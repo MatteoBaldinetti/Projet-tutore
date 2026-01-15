3d40fe9903f77a2884fd9654f9b99ff2a35199cf
Ken DINH <ken.dinh@edu.univ-eiffel.fr>
2026-01-15 10:23:36 +0100

    Initial commit

---
+++ api/src/main/java/com/agora/mmireserve/MmireserveApplication.java
@@ -0,0 +1,13 @@
+package com.agora.mmireserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmireserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmireserveApplication.class, args);
+	}
+
+}
