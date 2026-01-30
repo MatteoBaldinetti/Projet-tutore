113dbc29fad272b3abe515b8ba84d6f726373ae3
Vincent ROURE <60276541+ZIK3Y@users.noreply.github.com>
2026-01-30 11:02:07 +0100

    Adding Header and Footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
+        <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent, ChangeEvent } from "react";
+import { useState, type FormEvent, type ChangeEvent } from "react";
 import "../styles/Login.css";
 
 const Login = () => {
